94d5bb1be90854943080ba58ca26209b6fc3ae0d
gpaul988 <gpaul988gmail.com>
2024-01-15 12:07:48 +0100

    completed

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Graham S. Paul - 0-minoperations.py
+
+"""
+
+
+def minOperations(n):
+    """Retrieve the minimum operations required to archieve n characters."""
+    if n <= 1:
+        return 0
+    else:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                return minOperations(n // i) + i
+        return n
+
+
+if __name__ == "__main__":
+    n = 4
+    print(
+        "Min # of operations to reach {} char: {}".format(n, minOperations(n))
+            )
+
+    n = 12
+    print(
+        "Min # of operations to reach {} char: {}".format(n, minOperations(n))
+            )
